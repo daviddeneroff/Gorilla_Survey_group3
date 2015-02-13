4b36e86616671d02207a50d82e66e3252531f511
David Deneroff <dgdeneroff@gmail.com>
2015-02-13 12:20:58 -0500

    basic login/signup page

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,11 +1,16 @@
 get '/' do
-  erb :welcome
+  redirect '/login'
 end
 
 get '/login' do
-  
+  erb :login
 end
 
-get '/signup' do
-  
+post '/login' do
+  if @user = User.find_by(name: params[:user][:name]).try(:authenticate, params[:user][:password])
+    session[:user_id] = @user.id
+    redirect "/posts"
+  else
+    404
+  end
 end
