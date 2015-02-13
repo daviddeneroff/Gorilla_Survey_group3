9b32b909bd7dde7e1697ff7c65ecd35ac57f4470
Justin Gaba <gabalicious113@gmail.com>
2015-02-13 18:21:08 -0500

    adds survey to db

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -13,12 +13,20 @@ get '/surveys' do
 end
 
 get '/surveys/new' do
-  erb :'survey/survey_form'
+  erb :'survey/question_form'
+end
+
+get '/surveys/new/2' do
+  @count = params[:num_questions].to_i
+  erb :'survey/choices_form'
 end
 
 post '/surveys/new' do
-  Survey.create(params)
-  redirect '/user'
+  # {"survey_name"=>"test this one", "question"=>{"0"=>"number one", "1"=>"question 2", "2"=>"question"}, "choice1"=>{"0"=>"a1 q1", "1"=>"a1 q 2", "2"=>"a1 a3"}, "choice2"=>{"0"=>"a2 q1", "1"=>"a1 q2", "2"=>"a2 q 3"}}
+  # use params to make a survey object with qs and pas
+  Survey.create(title: params[:survey_name], user_id: current_user.id)
+
+  redirect '/surveys'
 end
 
 get '/surveys/:id' do
@@ -29,13 +37,13 @@ end
 post "/surveys/:id" do
   # @survey = Survey.find(params[:id])
   p params
-  redirect "/yolo"
+  redirect "/surveys"
 end
 
 
-get 'survey/:id/edit' do
-  erb :'survey/survey_form'
-end
+# get 'survey/:id/edit' do
+#   erb :'survey/survey_form'
+# end
 
 
 
