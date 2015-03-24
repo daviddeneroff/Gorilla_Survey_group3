304fb57301914a8487dca4bfa3bf1f57a415faa5
David Deneroff <dgdeneroff@gmail.com>
2015-03-24 18:23:22 -0400

    Made votes for multiple questions

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -14,34 +14,13 @@ get '/surveys' do
   erb :'survey/all_surveys'
 end
 
-# get '/surveys/new' do
-#   erb :'survey/question_form'
-# end
-
-# get '/surveys/new/2' do
-#   @count = params[:num_questions].to_i
-#   erb :'survey/choices_form'
-# end
-
-# post '/surveys/new' do
-#   questions = params[:question]
-#   new_survey = Survey.create(title: params[:survey_name], user_id: current_user.id)
-#     questions.each do |key, question|
-#       new_question = Question.create(survey_id: new_survey.id, title:question)
-#         PossibleChoice.create(question_id: new_question.id, content: params[:choice1][key], survey_id: new_survey.id)
-#         PossibleChoice.create(question_id: new_question.id, content: params[:choice2][key], survey_id: new_survey.id)
-#     end
-#   redirect '/surveys'
-# end
-
 get '/surveys/:id' do
   @survey = Survey.find(params[:id])
   erb :'survey/survey'
 end
 
 post "/surveys/:id" do
-  p params
-  Vote.create(possible_choice_id: params[:key])
+  params[:hello].each {|key, opts| Vote.create(possible_choice_id: opts) }
   redirect "/surveys"
 end
 
